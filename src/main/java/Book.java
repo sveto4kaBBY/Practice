c7e494965a7025eec7011045c3fde14744340dd8
sveto4kaBBY <svetkorneeva0404@gmail.com>
2025-07-10 16:31:41 +0300

    add program

---
+++ src/main/java/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    private final String id;
+    private final String title;
+    private final String author;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " (ID: " + id + ")";
+    }
+}
+
+
